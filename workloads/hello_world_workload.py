8b387d9295d3fd963cf18f551343a0c56f17896e
Mahyar Samani <msamani@ucdavis.edu>
2023-01-10 14:46:18 -0800

    Removing unnecessary import.

--- workloads/hello_world_workload.py
+++ workloads/hello_world_workload.py
@@ -24,7 +24,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from gem5.resources.resource import Resource, CustomResource
+from gem5.resources.resource import Resource
 
 from custom_se_workload import CustomSEWorkload
 
