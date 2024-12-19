41ad8ddaf973f4300ff28a09b4522b749cbc6ffd
Jason Lowe-Power <jason@lowepower.com>
2024-12-19 09:17:03 -0800

    misc: Fix paths for resources
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -13,3 +13,12 @@ git config --global blame.ignoreRevsFile .git-blame-ignore-revs
 ./util/pre-commit-install.sh
 
 popd # gem5
+
+# Update path in gem5-config and gem5-resources
+# replace instances of "/workspaces/gem5-assignment-template" with $BASE_PATH
+
+# Update path in gem5-config
+sed -i "s|/workspaces/gem5-assignment-template|$BASE_PATH|g" $BASE_PATH/gem5-config.json
+
+# Update path in gem5-resources
+sed -i "s|/workspaces/gem5-assignment-template|$BASE_PATH|g" $BASE_PATH/workloads/resources.json
