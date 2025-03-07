1f832c0f5c5a93166973f6ac779fb74bf3392eef
Jason Lowe-Power <jason@lowepower.com>
2025-03-07 19:24:39 +0000

    Update components for false-sharing for gem5 24.1
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- assignments/false-sharing/components/__init__.py
+++ assignments/false-sharing/components/__init__.py
@@ -8,7 +8,7 @@ from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_processor import SimpleProcessor
 from gem5.isas import ISA
 
-from cache_hierarchies import MESITwoLevelCacheHierarchy
+from .cache_hierarchies import MESITwoLevelCacheHierarchy
 
 X86Board = SimpleBoard
 
